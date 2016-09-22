1b2f40f538454c314b46480e19f0b30533f914a0
Matthias Krebs <matthias@aerotain.com>
2016-09-22 13:09:37 +0200

    Add information about our (skye) pozyx network IDs

---
+++ skye_pozyx_id.h
@@ -0,0 +1,18 @@
+#ifndef SKYE_POZYX_ID
+#define SKYE_POZYX_ID
+
+#define TAG_1_ID      0x6F35
+#define TAG_2_ID      0x6F38
+#define TAG_3_ID      0x6F39
+#define TAG_4_ID      0x6F3A
+#define TAG_5_ID      0x6F61
+#define TAG_6_ID      0x6F66
+#define ANCHOR_1_ID   0x6F5C
+#define ANCHOR_2_ID   0x6F47
+#define ANCHOR_3_ID   0x6F3F
+#define ANCHOR_4_ID   0x6F31
+#define ANCHOR_5_ID   0x6F5B
+#define ANCHOR_6_ID   0X6F4D
+
+
+#endif //SKYE_POZYX_ID
